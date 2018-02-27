460588ca554dc8a37ce9941f3d3cbe21ae19ea76
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-02-27 15:08:47 +0100

    made first test run

--- src/tests/roman-numerals-conversion.js
+++ src/tests/roman-numerals-conversion.js
@@ -1,4 +1,12 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(require('sinon-chai'));
+
+
+const r2a = (number) => 1
 
 describe('roman to arabic', () => {
-  it('I => 1',() => expect(r2a('I').to.equal(1)))
+  it('I => 1', () => expect(r2a('I')).to.equals(1));
 });
